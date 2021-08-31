ee66cffc834aa60bab62da79c8572f78fa90eca4
bjur781 <bjur781@aucklanduni.ac.nz>
2021-08-31 18:15:07 +1200

    feat
    added checking for winners
    Now a complete 2 player game

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -13,11 +13,11 @@ def printBoard():
 	print('C  ' + board['C1'] + '|' + board['C2'] + '|' + board['C3'])
 
 def play(player):
-	pos=input("Make your turn: ")
+	pos=input("Make your turn player "+player+": ")
 	if pos in board:
 		if board[pos] == ' ':
 			#allowed input
-			if player == 1:
+			if player == "1":
 				board[pos] = 'X'
 			else:
 				board[pos] = 'O'
@@ -28,12 +28,45 @@ def play(player):
 		print("That is not a valid position")
 		play(player)
 
-#size=input("Enter the size of the board (e.g. 3x3 = 3) (3-9) ")
-player = 1
-while True:
+def checkWinner(player, count):
+	#horizontals
+	if board['A1'] == board['A2'] == board['A3'] != ' ':
+		print("Player " + player + " is the winner!")
+	elif board['B1'] == board['B2'] == board['B3'] != ' ':
+		print("Player " + player + " is the winner!")
+	elif board['C1'] == board['C2'] == board['C3'] != ' ':
+		print("Player " + player + " is the winner!")
+	#verticals
+	elif board['A1'] == board['B1'] == board['C1'] != ' ':
+		print("Player " + player + " is the winner!")
+	elif board['A2'] == board['B2'] == board['C2'] != ' ':
+		print("Player " + player + " is the winner!")
+	elif board['A3'] == board['B3'] == board['C3'] != ' ':
+		print("Player " + player + " is the winner!")
+	#diagonals
+	elif board['A1'] == board['B2'] == board['C3'] != ' ':
+		print("Player " + player + " is the winner!")
+	elif board['A3'] == board['B2'] == board['C1'] != ' ':
+		print("Player " + player + " is the winner!")
+	#tie
+	elif count == 9:
+		print("You have tied")
+	#no winner yet
+	else:
+		start(player, count)
+
+def start(player, count):
 	printBoard()
 	play(player)
-	if player == 1:
-		player = 2
+	count = count + 1
+	if player == "1":
+		player = "2"
 	else:
-		player = 1
+		player = "1"
+	checkWinner(player, count)
+
+
+#size=input("Enter the size of the board (e.g. 3x3 = 3) (3-9) ")
+player = "1"
+count = 0
+start(player, count)
